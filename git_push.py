0ce8c28ef2b42eacff735e0cd851a3153dd100f2
Maxim Makarov <m.makarov@corp.mail.ru>
2020-05-14 19:45:09 +0300

    first commit

---
+++ git_push.py
@@ -0,0 +1,21 @@
+import time
+from git import Repo
+
+
+def git_push(file_list, commit_message):
+    repo = Repo('.')
+    for file in file_list:
+        repo.index.add(file)
+    repo.index.commit(commit_message)
+    origin = repo.remotes.origin
+    try:
+        origin.pull()
+        origin.push()
+    except Exception as error:
+        print(error)
+        time.sleep(300)
+        git_push(file_list, commit_message)
+
+
+git_push(['test.txt'], 'hello world commit']
+
